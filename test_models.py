9772983605b48478b12dd85b370169740464178d
ameni chakhari <ameni.chakhari@ensit.u-tunis.tn>
2026-03-30 09:22:41 +0100

    Remove venv and pycache

--- test_models.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-model   = joblib.load('ml_models/difficulty_model.pkl')
-scaler  = joblib.load('ml_models/scaler.pkl')
-encoder = joblib.load('ml_models/difficulty_encoder.pkl')
-
-print('✅ Model loaded:  ', type(model))
-print('✅ Scaler loaded: ', type(scaler))
-print('✅ Encoder loaded:', encoder.classes_)
