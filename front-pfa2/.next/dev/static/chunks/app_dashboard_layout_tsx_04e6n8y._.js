2c1e2fd6f9bce509537c188e3219ffd2bbc2ff61
sellami mohammed aziz <azizsellami22@gmail.com>
2026-04-20 21:07:22 +0100

    Delete front-pfa2 directory

--- front-pfa2/.next/dev/static/chunks/app_dashboard_layout_tsx_04e6n8y._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0juk-dr._.js"],
-    source: "dynamic"
-});
--- front-pfa2/.next/dev/static/chunks/app_dashboard_training_page_tsx_0sp0p71._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_02qu853._.js","static/chunks/_0zmq~lv._.js"],
-    source: "dynamic"
-});
