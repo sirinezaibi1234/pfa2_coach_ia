7013035b89a1dd4f015d9130c77fe0e78a9ed5c8
ameni chakhari <ameni.chakhari@ensit.u-tunis.tn>
2026-04-23 22:59:04 +0100

    fix some logics in nutrition services and routes

--- app/services/nutrition_service.py
+++ app/services/nutrition_service.py
@@ -3,37 +3,39 @@ import os
 
 class NutritionService:
     def __init__(self):
-        # On récupère les clés depuis le .env
         self.app_id = os.getenv("EDAMAM_APP_ID")
         self.app_key = os.getenv("EDAMAM_APP_KEY")
         self.base_url = "https://api.edamam.com/api/food-database/v2/parser"
-        print(f"DEBUG - ID: {self.app_id}, KEY: {self.app_key}")
 
     def fetch_nutrition(self, food_name):
-        # 1. Vérification de la présence des clés
         if not self.app_id or not self.app_key:
             return {"error": "Cles API manquantes dans le .env"}
 
-        # 2. Préparation des paramètres
         params = {
             "app_id": self.app_id,
             "app_key": self.app_key,
             "ingr": food_name
         }
         
-        # Debug visuel dans votre terminal pour vérifier l'URL générée
-        print(f"DEBUG - URL appelee: {self.base_url}?app_id={self.app_id}&ingr={food_name}")
-        
-        # 3. Requête à l'API
         try:
             response = requests.get(self.base_url, params=params)
-            # Renvoie le JSON si succès (200), sinon renvoie l'erreur détaillée
+            
             if response.status_code == 200:
-                return response.json()
+                data = response.json()
+                
+                # C'EST ICI QUE ÇA CHANGE :
+                # On ne renvoie pas tout le JSON brut, on extrait les 3 piliers
+                if data.get('hints'):
+                    first_hint = data['hints'][0]
+                    return {
+                        "label": first_hint['food'].get('label'),
+                        "nutrients": first_hint['food'].get('nutrients'), # Micros + Macros
+                        "measures": first_hint.get('measures')           # Unités de mesure
+                    }
+                else:
+                    return {"error": "Aliment non trouvé dans la base Edamam"}
             else:
-                return {
-                    "error": f"API Error {response.status_code}",
-                    "details": response.text
-                }
+                return {"error": f"API Error {response.status_code}", "details": response.text}
+                
         except Exception as e:
             return {"error": "Connection Error", "details": str(e)}
